8e912bcbcf93715c1cd68f09e613df875cccc003
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-08-07 16:56:24 +0300

    configured initialization of gui

--- src/main/java/com/example/librarymanagementservice/LibraryApplication.java
+++ src/main/java/com/example/librarymanagementservice/LibraryApplication.java
@@ -2,22 +2,21 @@ package com.example.librarymanagementservice;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+public class LibraryApplication extends Application {
 
-public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("library.fxml"));
+        primaryStage.setTitle("Library Management System");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
