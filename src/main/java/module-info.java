47d40c13ab614787a1edcd3fd3565eee84b45eb6
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-08-07 18:49:55 +0300

    dependency management

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,10 @@ module com.example.librarymanagementservice {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
+    requires static lombok;
 
     opens com.example.librarymanagementservice to javafx.fxml;
     exports com.example.librarymanagementservice;
+    exports com.example.librarymanagementservice.exceptions;
+    opens com.example.librarymanagementservice.exceptions to javafx.fxml;
 }
